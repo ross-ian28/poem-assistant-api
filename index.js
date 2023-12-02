0ec442b9d090000732c14f9ef2830b347312e536
Ian Ross <ianrossdrums@gmail.com>
2023-12-02 13:07:31 -0700

    refactor prompt generator

--- index.js
+++ index.js
@@ -21,14 +21,13 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 app.post('/prompt-generator', async (req, res) => {
-  console.log("request body: ", req.body);
   const { amount } = req.body;
 
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
       { role: 'system', content: 'You are a helpful assistant creating poem prompts.' },
-      { role: 'user', content: `Create ${amount} prompt(s) for a short poem` },
+      { role: 'user', content: `Create ${amount} prompt for a short poem` },
       { role: 'user', content: 'Order the prompt(s) in a list like so based on the amount of prompts requested: 1) "Refer to several different beverages" 2) "Include a childhood memory" These two are just examples, dont use them' }
     ]
   });
@@ -39,6 +38,62 @@ app.post('/prompt-generator', async (req, res) => {
   } else {
     res.json({ message: 'Error retrieving response' });
   }
+});
+
+app.post('/grammer-checker', async (req, res) => {
+    const { message } = req.body;
+    
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+        { role: 'system', content: 'You are a poem writing assistant whos job is to help check the grammer of the text given' },
+        { role: 'user', content: `Take this text: ${message}. And check for any grammer errors and places where the grammer can be improved` },
+        { role: 'user', content: `After create a sepreate response for any spelling errors` }
+        ]
+    });
+    
+    if (response.data.choices[0]) {
+        res.json({ message: response.data.choices[0].message.content });
+    } else {
+        res.json({ message: 'Error retrieving response' });
+    }
+});
+
+app.post('/thesaurus', async (req, res) => {
+    const { word } = req.body;
+    
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+        { role: 'system', content: 'You are a thesaurus giving words similiar to the word provided' },
+        { role: 'user', content: `Take this word: ${word}. And give 10 synonyms and antonyms as example` }
+        ]
+    });
+    
+    if (response.data.choices[0]) {
+        res.json({ message: response.data.choices[0].message.content });
+    } else {
+        res.json({ message: 'Error retrieving response' });
+    }
+});
+
+app.post('/word-generator', async (req, res) => {
+    const { amount } = req.body;
+    
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+        { role: 'system', content: 'You are a helpful assistant creating random words to inspire use in a poem.' },
+        { role: 'user', content: `Create ${amount} word(s) to use in a short poem` },
+        { role: 'user', content: 'Order the word(s) in a list like so based on the amount of words requested: 1) "Happy" 2) "Beautiful" These two are just examples, dont use them' }
+        ]
+    });
+    
+    if (response.data.choices[0]) {
+        res.json({ message: response.data.choices[0].message.content });
+    } else {
+        res.json({ message: 'Error retrieving response' });
+    }
 });
 
 app.listen(
