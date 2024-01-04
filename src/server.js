9fbf7ab30c2aa7af340a84fbbe4fde656b9ac59e
Ian Ross <ianrossdrums@gmail.com>
2024-01-03 17:36:29 -0700

    Update mongoose and remove mongo requests

--- src/server.js
+++ src/server.js
@@ -5,9 +5,14 @@ const OpenAI = require("openai");
 const { Configuration, OpenAIApi } = OpenAI;
 
 // Define mongodb dependencies
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://admin:${process.env.MONGO_STRING}@poem-assistant-database.jtabmts.mongodb.net/?retryWrites=true&w=majority`)
-const UserModel = require('../src/models/Users.js');
+// const mongoose = require('mongoose');
+// mongoose.connect(`mongodb+srv://admin:${process.env.MONGO_PASS}@poem-assistant-database.jtabmts.mongodb.net/?retryWrites=true&w=majority`)
+// const UserSchema = new mongoose.Schema({
+//     username: { type: String, required: true },
+//     password: { type: String, required: true },
+//     notes: { type: Array, required: false }
+// });
+// const UserModel = mongoose.model("poem-assistant-collection", UserSchema)
 
 // Define port
 const express = require('express');
@@ -141,23 +146,23 @@ app.post('/search', async (req, res) => {
 });
 
 // Get all users in database
-app.get('/users', (req, res) => {
-    UserModel.find({}, (error, result) => {
-        if (error) {
-            res.json({ message: `Error retrieving response: ${error}` });
-        } else {
-            res.json({ message: result });
-        }
-    })
-});
+// app.get('/users', (req, res) => {
+//     UserModel.find({}, (error, result) => {
+//         if (error) {
+//             res.json({ message: `Error retrieving response: ${error}` });
+//         } else {
+//             res.json({ message: result });
+//         }
+//     })
+// });
 
-app.post("/register", async (req, res) => {
-    const user = req.body;
-    const newUser = new UserModel(user);
-    await newUser.save();
+// app.post("/register", async (req, res) => {
+//     const user = req.body;
+//     const newUser = new UserModel(user);
+//     await newUser.save();
 
-    res.json(user);
-})
+//     res.json(user);
+// })
 
 module.exports = app;
 
