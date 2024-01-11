39ad0acca4e6c3122d1d48895a2bcb9ee95ed28e
Ian Ross <ianrossdrums@gmail.com>
2024-01-10 20:16:23 -0700

    update dependencies

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ app.post('/prompt-generator', async (req, res) => {
     model: "gpt-3.5-turbo",
     messages: [
       { role: 'system', content: 'You are a helpful assistant creating a specified amount of poem prompts.' },
-      { role: 'user', content: `Create ${amount} prompt for a short poem` },
+      { role: 'user', content: `Create "${amount}" prompt for a short poem` },
       { role: 'user', content: 'Order the prompt(s) in a list like so based on the amount of prompts requested: 1) "Refer to several different beverages" 2) "Include a childhood memory" These two are just examples, dont use them' }
     ]
   });
@@ -94,7 +94,7 @@ app.post('/word-generator', async (req, res) => {
         model: "gpt-3.5-turbo",
         messages: [
         { role: 'system', content: 'You are a helpful assistant creating random words to inspire use in a poem.' },
-        { role: 'user', content: `Create ${amount} word(s) to use in a short poem` },
+        { role: 'user', content: `Create "${amount}" word(s) to use in a short poem` },
         { role: 'user', content: 'Order the word(s) in a list like so based on the amount of words requested: 1) "Happy" 2) "Beautiful" These two are just examples, dont use them' }
         ]
     });
